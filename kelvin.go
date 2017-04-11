46b8305c1003b5959ef113916730b1a42a814863
Stefan Wichmann <mail@stefanwichmann.com>
2017-04-11 22:03:12 +0200

    Fix logging to file

--- kelvin.go
+++ kelvin.go
@@ -106,7 +106,7 @@ func configureLogging() {
 	if *debug {
 		log.SetLevel(log.DebugLevel)
 	}
-	if logfile != nil {
+	if logfile != nil && *logfile != "" {
 		file, err := os.OpenFile(*logfile, os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
 		if err == nil {
 			log.SetOutput(file)
