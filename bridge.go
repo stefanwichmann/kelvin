3860d0533c1ab62677a7a329cd536e6af5d2f38f
Stefan Wichmann <mail@stefanwichmann.com>
2017-12-05 18:36:04 +0100

    Update latest bridge version

--- bridge.go
+++ bridge.go
@@ -243,7 +243,7 @@ func (bridge *HueBridge) validateSofwareVersion() {
 	}
 	log.Debugf("⌘ Bridge is running software version %s", configuration.SoftwareVersion)
 
-	if (bridge.Version == 1 && swversion < 1038802) || (bridge.Version == 2 && swversion < 1709131301) {
+	if (bridge.Version == 1 && swversion < 1038802) || (bridge.Version == 2 && swversion < 1711151408) {
 		log.Warningf("⌘ Your hue bridge is running an old software version. Please update using the hue app to ensure Kelvin will run smoothly.")
 	} else {
 		log.Debugf("⌘ Bridge software is up to date")
